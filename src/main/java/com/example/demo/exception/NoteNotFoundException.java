d37be4aadc4a87ee38e86b047cb71a7966a57f49
Aleksandr Rogozhkin <rogozkin371@gmail.com>
2023-08-25 15:57:41 +0300

    Made NoteService and NoteNotFoundException

--- src/main/java/com/example/demo/exception/NoteNotFoundException.java
+++ src/main/java/com/example/demo/exception/NoteNotFoundException.java
@@ -1,2 +1,12 @@
-package com.example.demo.exception;public class NoteNotFoundException {
+package com.example.demo.exception;
+
+import java.util.UUID;
+
+public class NoteNotFoundException extends Exception{
+    private static final String NOTE_NOT_FOUND_EXCEPTION_TEXT = "Note with id = %s not found.";
+
+
+    public NoteNotFoundException(UUID noteId) {
+        super(String.format(NOTE_NOT_FOUND_EXCEPTION_TEXT, noteId));
+    }
 }
