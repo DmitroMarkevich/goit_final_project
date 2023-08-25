4abd858397a25a92b092727be3ad6f4ab87ca1d5
Dmytro Markevych <markevicdmitro003@gmail.com>
2023-08-25 16:07:25 +0300

    Added UserDto and NoteDto

---
+++ src/main/java/com/example/demo/note/NoteDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.note;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@Builder
+public class NoteDto {
+    private UUID id;
+    private Timestamp createdAt;
+    private Timestamp updatedAt;
+    private String title;
+    private String content;
+    private AccessType accessType;
+    private UUID userId;
+}
