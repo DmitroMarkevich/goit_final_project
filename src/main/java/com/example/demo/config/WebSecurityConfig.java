a752a11ce5720d1234e42f9bd882e44da81718b9
Dmytro Markevych <markevicdmitro003@gmail.com>
2023-08-26 20:16:33 +0300

    Off cors & csrf defense in WebSecurityConfig

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,6 +14,8 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/", "/login", "/register").permitAll();
                     authorize.anyRequest().authenticated();
