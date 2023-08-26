a9e1df1cf79435ced52f3431ce6672fb4074e540
Dmytro Markevych <markevicdmitro003@gmail.com>
2023-08-26 11:13:56 +0300

    Fixed NoteValidator

--- src/main/java/com/example/demo/note/NoteValidator.java
+++ src/main/java/com/example/demo/note/NoteValidator.java
@@ -1,21 +1,27 @@
 package com.example.demo.note;
 
-import com.example.demo.exception.note.NoteValidationException;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 
 @Component
-public class NoteValidator {
-    public void validate(NoteDto noteDto) throws NoteValidationException {
-        if (!isValidTitle(noteDto.getTitle()) || !isValidContent(noteDto.getContent())) {
-            throw new NoteValidationException(noteDto.getId());
+public class NoteValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return NoteDto.class.equals(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        NoteDto noteDto = (NoteDto) target;
+        String noteContent = noteDto.getContent();
+        String noteTitle = noteDto.getTitle();
+
+        if (noteContent.length() <= 500 && noteContent.length() >= 5) {
+            errors.rejectValue("content", "note.content.invalid", "Помилка - введіть вміст нотатки від 5 до 500 символів!");
         }
-    }
-
-    private boolean isValidTitle(String noteTitle) {
-        return noteTitle.length() <= 100 && noteTitle.length() >= 5;
-    }
-
-    private boolean isValidContent(String noteContent) {
-        return noteContent.length() <= 500 && noteContent.length() >= 5;
+        if (noteTitle.length() <= 100 && noteTitle.length() >= 5) {
+            errors.rejectValue("title", "note.title.invalid", "Помилка - введіть назву нотатки від 5 до 100 символів!");
+        }
     }
 }
--- src/main/java/com/example/demo/user/UserValidator.java
+++ src/main/java/com/example/demo/user/UserValidator.java
@@ -5,29 +5,29 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
-
 @Component
 public class UserValidator implements Validator {
     @Override
     public boolean supports(Class<?> clazz) {
         return UserDto.class.equals(clazz);
     }
+
     @Override
     public void validate(Object target, Errors errors) {
-    UserDto userDto = (UserDto) target;
+        UserDto userDto = (UserDto) target;
 
-    if (userDto.getUsername().length() < 5 || userDto.getUsername().length() > 50) {
-        errors.rejectValue("username", "user.username.invalid", "Помилка - введіть логін від 5 до 50 символів!");
-    } else if (!userDto.getUsername().matches("^[a-zA-Z0-9]+$")) {
-        errors.rejectValue("username", "user.username.invalidchars", "Логін може містити лише латинські букви та цифри!");
-    }
-    if (userDto.getPassword().length() < 8 || userDto.getPassword().length() > 100) {
-        errors.rejectValue("password", "user.password.invalid", "Невірний пароль. Введіть не менше 8 символів!");
-    }
+        if (userDto.getUsername().length() < 5 || userDto.getUsername().length() > 50) {
+            errors.rejectValue("username", "user.username.invalid", "Помилка - введіть логін від 5 до 50 символів!");
+        } else if (!userDto.getUsername().matches("^[a-zA-Z0-9]+$")) {
+            errors.rejectValue("username", "user.username.invalidchars", "Логін може містити лише латинські букви та цифри!");
+        }
+        if (userDto.getPassword().length() < 8 || userDto.getPassword().length() > 100) {
+            errors.rejectValue("password", "user.password.invalid", "Невірний пароль. Введіть не менше 8 символів!");
+        }
 
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", "user.email.empty", "Заповніть поле електронної пошти!");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "firstName", "user.firstName.empty", "Введіть Ваше ім'я");
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "lastName", "user.lastName.empty", "Введіть Ваше прізвище!");
-
-}}
+    }
+}
 
