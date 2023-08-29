bcad1766f5fc98d336706b7e9c3b9794128ad082
Dmytro Markevych <markevicdmitro003@gmail.com>
2023-08-29 11:33:13 +0300

    Implemented Login and Password Authentication for Spring Security

--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -1,18 +1,9 @@
 package com.example.demo.user;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService  {
-    private final UserRepository userRepository;
-
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//
-//    }
 }
